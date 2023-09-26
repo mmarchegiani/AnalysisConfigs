7affb3176b2402cb2e63099d595e69704990f907
Roberto Seidita <roberto.seidita@cern.ch>
2023-09-26 17:49:13 +0200

    First attempt at LAW workflow

---
+++ configs/test_law/law_tasks.py
@@ -0,0 +1,34 @@
+import os
+
+import luigi
+import law
+import json
+
+class TaskBase(law.Task):
+    
+    config_dir = luigi.Parameter(default="/work/rseidita/CAT_hackathon/AnalysisConfigs/configs/test_law")
+    datasets_dir = luigi.Parameter(default="/work/rseidita/CAT_hackathon/AnalysisConfigs/configs/test_law/datasets")
+    datasets_definition = luigi.Parameter(default="/work/rseidita/CAT_hackathon/PocketCoffea/datasets/datasets_definitions_example.json")
+
+class CreateDataset(TaskBase):
+    
+    def read_datasets_definition(self, path):
+        with open(path, "r") as f:
+            return json.load(f)
+    
+    def output(self):
+        datasets = self.read_datasets_definition(self.datasets_definition)
+        dataset_paths = set()
+        for dataset in datasets.values():
+            dataset_paths.add(f"{self.datasets_dir}/{dataset['json_output']}")
+            dataset_paths.add(f"{self.datasets_dir}/{dataset['json_output']}".replace(".json", "_redirector.json"))
+            
+        return [law.LocalFileTarget(d) for d in dataset_paths]
+    
+    def run(self):
+        os.system(f"build_datasets.py --cfg {self.datasets_definition}")
+
+class Runner(TaskBase):
+    
+    def requires(self):
+        return CreateDataset.req(self)
