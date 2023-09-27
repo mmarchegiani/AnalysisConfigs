caa8e8e5be201fbf822743dcc586c8cf25cbf805
mmarchegiani <matmarcheg@gmail.com>
2023-09-27 16:25:06 +0200

    Update law tasks

--- configs/test_law/law_tasks.py
+++ configs/test_law/law_tasks.py
@@ -4,12 +4,16 @@ import luigi
 import law
 import json
 
+from pocket_coffea.utils.dataset import build_datasets
+
 class TaskBase(law.Task):
     
     cfg = luigi.Parameter()
     datasets_definition = luigi.Parameter()
 
 class CreateDataset(TaskBase):
+
+    keys = luigi.Parameter()
 
     def read_datasets_definition(self, path):
         with open(path, "r") as f:
@@ -26,7 +30,8 @@ class CreateDataset(TaskBase):
         return [law.LocalFileTarget(d) for d in dataset_paths]
     
     def run(self):
-        os.system(f"build_datasets.py --cfg {self.datasets_definition}")
+        args = {'cfg' : self.datasets_definition}
+        build_datasets(**args)
 
 class Runner(TaskBase):
 
@@ -36,7 +41,31 @@ class Runner(TaskBase):
         return CreateDataset.req(self)
 
     def output(self):
-        return law.LocalFileTarget(os.path.join(self.output_dir, "output_all.coffea"))
+        required_files = [os.path.join(self.output_dir, "output_all.coffea"), os.path.join(self.output_dir, "parameters_dump.yaml")]
+        return [law.LocalFileTarget(file) for file in required_files]
 
     def run(self):
         os.system(f"runner.py --cfg {self.cfg} -o {self.output_dir} --full --test -lf 1")
+
+class Plotter(TaskBase):
+
+    output_dir = luigi.Parameter(default=os.path.join(os.getcwd(), "test"))
+
+    def requires(self):
+        return Runner.req(self)
+
+    def output(self):
+        # Here we should define the list of the output files of plots
+        pass
+
+    run_plots = False
+    def complete(self):
+        if self.run_plots == True:
+            return True
+        else:
+            return False
+
+    def run(self):
+        config_dir = os.path.abspath(os.path.dirname(self.cfg))
+        os.system(f"make_plots.py --cfg {self.output_dir}/parameters_dump.yaml -op {config_dir}/params/plotting_style.yaml -i {self.output_dir}/output_all.coffea -o {self.output_dir}/plots -j 8")
+        self.run_plots = True
